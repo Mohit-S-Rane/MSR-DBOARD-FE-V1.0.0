35b1fc04ef7ee50ee6d2affae1c5552406257389
Mohit-S-Rane <mohitsrane16@gmail.com>
2023-02-08 17:58:55 +0530

    Left Menu Icon added

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -13,11 +13,60 @@ export const defaultNavigation: FuseNavigationItem[] = [
 export const compactNavigation: FuseNavigationItem[] = [
     {
         id   : 'example',
-        title: 'Example',
+        title: 'Home',
+        type : 'basic',
+        icon : 'heroicons_outline:home',
+        link : '/example',
+        tooltip: 'Home'
+    },
+    {
+        id   : 'example',
+        title: 'Project',
+        type : 'basic',
+        icon : 'heroicons_outline:folder',
+        link : '/example',
+        tooltip: 'Project'
+    },
+    {
+        id   : 'example',
+        title: 'Everything',
+        type : 'basic',
+        icon : 'heroicons_outline:globe-alt',
+        link : '/example',
+        tooltip: 'Everything'
+    },
+    {
+        id   : 'example',
+        title: 'Inbox',
+        type : 'basic',
+        icon : 'heroicons_outline:mail',
+        link : '/example',
+        tooltip: 'Inbox'
+    },
+    {
+        id   : 'example',
+        title: 'Report',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
+        link : '/example',
+        tooltip: 'Report'
+    },
+    {
+        id   : 'example',
+        title: 'Portfolio',
+        type : 'basic',
+        icon : 'heroicons_outline:chart-bar',
+        link : '/example',
+        tooltip: 'Portfolio'
+    },
+    {
+        id   : 'example',
+        title: 'People',
+        type : 'basic',
+        icon : 'heroicons_outline:users',
+        link : '/example',
+        tooltip: 'People'
+    },
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
